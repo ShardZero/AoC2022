f14ea5fd02032768b9dc5a23a5dc6e6b3ddf3d2d
ShardZero <119866619+ShardZero@users.noreply.github.com>
2022-12-06 17:09:06 +0100

    Add files via upload

---
+++ Day5.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC2022.Days
+{
+    class Day5
+    {
+        Stack<char>[] Stacks;
+        List<string> Movements = new List<string>();
+        public static Day5 Current = new Day5();
+
+        private Day5()
+        {
+            StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "../../Data/5 - Supply Stack.txt");
+
+            List<string> StackStrings = new List<string>(); //temporäre Liste der Stackdaten
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (Stacks == null)
+                {
+                    if (line.Length != 0)
+                    {
+                        StackStrings.Add(line);
+                    }
+                    else
+                    {
+                        string temp = Regex.Replace(StackStrings[StackStrings.Count - 1], @"\s+", " "); //Mehrere spaces mit einem space ersetzen
+                        Stacks = new Stack<char>[temp.Trim().Split(' ').Length];
+                        for (int i = 0; i < Stacks.Length; i++)
+                            Stacks[i] = new Stack<char>();
+
+
+                        for (int i = StackStrings.Count-2; i >= 0; i--)
+                        {
+                            for(int j = 0; j < Stacks.Length; j++)
+                            {
+                                if (StackStrings[i][1 + 4 * j] != ' ')
+                                    Stacks[j].Push(StackStrings[i][1 + 4 * j]);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(line.Length > 0)
+                        Movements.Add(line);
+                }
+
+            }
+        }
+
+        public string Task1()
+        {
+            Stack<char>[] TempStack = new Stack<char>[Stacks.Length];
+            for (int i = 0; i < Stacks.Length; i++)
+                TempStack[i] = new Stack<char>(Stacks[i]);
+
+            foreach (string line in Movements)
+            {
+                string[] movesplit = line.Split(' ');
+                for(int i = 0; i < int.Parse(movesplit[1]); i++)
+                    TempStack[int.Parse(movesplit[5]) - 1].Push(TempStack[int.Parse(movesplit[3]) - 1].Pop());
+            }
+            string result = "";
+            foreach (Stack<char> tmp in TempStack)
+                result += tmp.Peek();
+            return "The End of the Stacks spell: " + result;
+        }
+
+        public string Task2()
+        {
+            Stack<char>[] TempStack = new Stack<char>[Stacks.Length];
+            for (int i = 0; i < Stacks.Length; i++)
+                TempStack[i] = Stacks[i];
+
+            foreach (string line in Movements)
+            {
+                string[] movesplit = line.Split(' ');
+                Stack<char> PufferStack = new Stack<char>();
+                for (int i = 0; i < int.Parse(movesplit[1]); i++)
+                   PufferStack.Push(TempStack[int.Parse(movesplit[3]) - 1].Pop());
+                while(PufferStack.Count() != 0)
+                    TempStack[int.Parse(movesplit[5]) - 1].Push(PufferStack.Pop());
+            }
+            string result = "";
+            foreach (Stack<char> tmp in TempStack)
+                result += tmp.Peek();
+            return "The End of the Stacks spell: " + result;
+        }
+    }
+}
---
+++ Day6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    class Day6
+    {
+        private string datastream;
+        public static Day6 Current = new Day6();
+
+        private Day6()
+        {
+            StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "../../Data/6 - Tuning Trouble.txt");
+            datastream = reader.ReadToEnd();
+            reader.Close();
+        }
+
+        public string Task1()
+        {
+            for(int i = 0; i < datastream.Length-4; i++)
+            {
+                if (datastream.Substring(i, 4).Distinct().Count() == 4)
+                    return "first marker after character " + (i + 4);
+            }
+            return "Nothing found";
+        }
+
+        public string Task2()
+        {
+            for (int i = 0; i < datastream.Length - 14; i++)
+            {
+                if (datastream.Substring(i, 14).Distinct().Count() == 14)
+                    return "first marker after character " + (i + 14);
+            }
+            return "Nothing found";
+        }
+    }
+}
